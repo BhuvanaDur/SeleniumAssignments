db095ed136d577d4aa35739707aa923b94b645ab
Bhuvaneswari <100114543+BhuvanaDur@users.noreply.github.com>
2023-06-26 18:44:30 +0530

    Add files via upload
    find intersection of array

---
+++ intersection.java
@@ -0,0 +1,20 @@
+package week1day2;
+
+public class intersection {
+
+	public static void main(String[] args) {
+				int[] arr1 = {3,2,11,4,6,7};
+		int[] arr2 = {1,2,8,4,9,7};
+		
+		for (int i=0;i<=5;i++) {
+			for (int j=0;j<=5;j++) {
+				if(arr1[i]==arr2[j]) {
+					System.out.println("Value:"+ " "+ arr1[i]);
+				
+				}
+				
+			}
+		}
+	}
+
+}
